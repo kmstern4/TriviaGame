209461689287954d8f4802fade44683acf6eaa79
kmstern4 <kmstern4@gmail.com>
2018-11-20 01:38:55 -0700

    added image to end screen

--- assets/javascript/new.js
+++ assets/javascript/new.js
@@ -59,42 +59,50 @@ var time = 30;
 var interval;
 
 function resetQuestion() {
-    $("#questionText").empty();
-    $("#questionText").text(questionsArr[index].question);
-    $("#choicesText").empty();
-    $("#gifText").empty();
-    var choices = questionsArr[index].answer;
-    console.log(choices);
-    for (i = 0; i < choices.length; i++) {
-        var newRadio = $("<input type='radio' name='choice' />");
-        var radioLabel = $("<label>").attr("for", i).text(choices[i]);
-        newRadio.attr("value", i).attr("id", i);
-        $("#choicesText").append(newRadio).append(radioLabel).append("<br>");
-    };
-    timerStart();
-    clickEvent();
+        $("#questionText").empty();
+        $("#questionText").text(questionsArr[index].question);
+        $("#choicesText").empty();
+        $("#gifText").empty();
+        var choices = questionsArr[index].answer;
+        for (i = 0; i < choices.length; i++) {
+            var newRadio = $("<input type='radio' name='choice' />");
+            var radioLabel = $("<label>").attr("for", i).text(choices[i]);
+            newRadio.attr("value", i).attr("id", i);
+            $("#choicesText").append(newRadio).append(radioLabel).append("<br>");
+        };
+        timerStart();
+        clickEvent();
 };
 
-resetQuestion();
+function startGame() {
+    if (index <= (questionsArr.length - 1)) {
+        resetQuestion();
+    } else {
+        $("#questionText").empty();
+        $("#choicesText").empty();
+        $("#gifText").empty();
+        $("p").text("You correctly guessed " + score + " questions!").append("<br>").append("<img src='assets/images/party.jpg'>");
+    }
+};
+
+startGame();
 
 
 function clickEvent() {
     $("input").on("click", function() {
-        console.log(this.value);
-        
         if (questionsArr[index].correct == this.value) {
             var newImg = $("<img>").attr("src", imgArr[index]);
             $("#gifText").append("<p id='correctP'>Correct!</p>").append(newImg);
             index++;
             score++;
             clearInterval(interval);
-            setTimeout(resetQuestion, 4000);
+            setTimeout(startGame, 4000);
         } else {
             var newImg = $("<img>").attr("src", imgArr[index]);
             $("#gifText").append("<p id='correctP'>Incorrect!</p>").append(newImg);
             index++;
             clearInterval(interval);
-            setTimeout(resetQuestion, 3500);
+            setTimeout(startGame, 3500);
         };
 
     });
@@ -108,7 +116,7 @@ function timerCount() {
             $("#gifText").append("<p id='correctP'>Out of time!</p>").append(newImg);
             index++;
             clearInterval(interval);
-            setTimeout(resetQuestion, 4000);
+            setTimeout(startGame, 4000);
     }
 };
 
